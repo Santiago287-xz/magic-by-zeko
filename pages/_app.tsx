50280be6bc606c815f0dd44596f409614ea42e03
Santiago287-xz <131275611+Santiago287-xz@users.noreply.github.com>
2023-12-04 18:00:13 -0300

    analytics vercel ✅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,23 @@ import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { fontSans, fontMono, outfit } from "@/config/fonts";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<NextUIProvider >
-			<NextThemesProvider attribute="class" defaultTheme="light" >
-				<Component {...pageProps} />
-			</NextThemesProvider>
-		</NextUIProvider>
-	);
+  return (
+    <>
+      <NextUIProvider>
+        <NextThemesProvider attribute="class" defaultTheme="light">
+          <Component {...pageProps} />
+        </NextThemesProvider>
+      </NextUIProvider>
+      <Analytics />
+    </>
+  );
 }
 
 export const fonts = {
-	sans: fontSans.style.fontFamily,
-	outfit: outfit.style.fontFamily,
-	mono: fontMono.style.fontFamily
+  sans: fontSans.style.fontFamily,
+  outfit: outfit.style.fontFamily,
+  mono: fontMono.style.fontFamily,
 };
